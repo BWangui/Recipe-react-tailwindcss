9b0f1aacc2fd3975cbe2f9445e6b286d8f71792d
BWangui <brigidtwangui@outlook.com>
2025-03-22 12:26:00 +0300

    removed imports of deleted files

--- recipe-finder-app/src/index.js
+++ recipe-finder-app/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './styles/tailwind.css'; // Import Tailwind CSS file
+import './index.css';
 import App from './App';
 
 
