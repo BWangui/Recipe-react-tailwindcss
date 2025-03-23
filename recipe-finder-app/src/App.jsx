bfe9b36baa2b8c9e3f97cebcb163620f41d86623
BWangui <brigidtwangui@outlook.com>
2025-03-23 16:15:53 +0300

    deleted

--- recipe-finder-app/src/App.jsx
+++ recipe-finder-app/src/App.jsx
@@ -1,24 +1,12 @@
-// App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SplashScreen from './SplashScreen';
-import HomeScreen from './HomeScreen';
-import CategoryScreen from './CategoryScreen';
-
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Splash Screen route */}
-        <Route path="/" element={<SplashScreen />} />
-        {/* Home Screen route */}
-        <Route path="/home" element={<HomeScreen />} />
-        {/* Category Screen route; the ":name" parameter holds the category name */}
-        <Route path="/category/:name" element={<CategoryScreen />} />
-      </Routes>
-    </Router>
+    <div className='bg-black'>
+      <Home/>
+    </div>
   );
+   
 }
 
 export default App;
