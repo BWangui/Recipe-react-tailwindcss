0afc56aa608c65b3c6b6bd8ecb4bbcc9bc0fec5d
BWangui <brigidtwangui@outlook.com>
2025-03-26 12:35:09 +0300

    commit file

---
+++ fast-feast-recipes/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Categories from '../components/Categories'
+import SearchBar from '../components/SearchBar'
+
+const Home =() => {
+    return (
+        <>
+        <SearchBar/>
+        <Categories/>
+        </>
+    )
+}
+export default Home
