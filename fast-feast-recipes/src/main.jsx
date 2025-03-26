1d96d324c33b84a45cd9e1adbc2eeced2fee1ca8
BWangui <brigidtwangui@outlook.com>
2025-03-26 10:33:44 +0300

    deleted

--- fast-feast-recipes/src/main.jsx
+++ fast-feast-recipes/src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router"; // Import the router
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
