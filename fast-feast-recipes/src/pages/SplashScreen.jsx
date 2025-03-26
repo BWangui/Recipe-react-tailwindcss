401bf5ea5e5f88abc631c7f13598ddc9b874645c
BWangui <brigidtwangui@outlook.com>
2025-03-26 09:26:42 +0300

    modified the files by adding interaction features

--- fast-feast-recipes/src/pages/SplashScreen.jsx
+++ fast-feast-recipes/src/pages/SplashScreen.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/logo.png"; // Import the logo
+import logo from "../assets/logo.jpeg"; // Import the logo
 
 const SplashScreen = () => {
   const navigate = useNavigate();
@@ -15,11 +15,11 @@ const SplashScreen = () => {
 
   return (
     <div 
-      className="relative flex items-center justify-center h-screen bg-cover bg-center" 
-      style={{ backgroundImage: "url('/background image,jpg')" }} // Background Image
+      className="flex items-center justify-center h-screen bg-cover-blue-500" 
+      style={{ backgroundImage: "url('/background image.jpg')" }} // Background Image
     >
       {/* Logo at the Top Left */}
-      <img src={logo} alt="Fast Feast Logo" className="absolute top-4 left-4 w-24 animate-fadeIn" />
+      <img src="logo.jpeg" alt="Fast Feast Logo" className="absolute top-4 left-4 w-24 animate-fadeIn" />
 
       {/* App Name with Move-in Effect */}
       <h1 className="text-white text-4xl font-bold animate-moveIn">Fast Feast Recipes</h1>
