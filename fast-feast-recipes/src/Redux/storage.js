8439f62d5e57c9a7f24faa7a886fd67602350851
BWangui <brigidtwangui@outlook.com>
2025-03-27 10:11:36 +0300

    deleted

--- fast-feast-recipes/src/Redux/storage.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from "../redux/slices/cartslice"
-
-export const store = configureStore({
-    reducer: {
-        cart:cartReduce
-    },
-})
